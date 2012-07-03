5869cb5f502ade67bcfcb62c4038c2e6bc38ed4d
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-07-03 11:31:12 +0200

    Checked in initial version.

---
+++ commons-io/src/test/scala/de/schlichtherle/commons/io/LoanSpec.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+package de.schlichtherle.commons.io
+
+import java.io._
+import org.junit.runner._
+import org.scalatest._
+import org.scalatest.junit._
+import org.scalatest.matchers._
+import org.scalatest.mock._
+import org.mockito.Mockito._
+
+@RunWith(classOf[JUnitRunner])
+class LoanSpec extends WordSpec with ShouldMatchers with MockitoSugar {
+  import Loan._
+
+  "A Loan" should {
+    "call AutoCloseable.close()" when {
+      "returning" in {
+        val resource = mock[AutoCloseable]
+        Loan(resource) { _ => }
+        verify(resource) close ()
+      }
+
+      "throwing a Throwable" in {
+        val ex = new Exception
+        val resource = mock[AutoCloseable]
+        intercept[Exception] (loan (resource) to { _ => throw ex }) should be theSameInstanceAs ex
+        verify(resource) close ()
+      }
+    }
+
+    "suppress any Throwable thrown by AutoCloseable.close()" in {
+      val ex = new Exception
+      val resource = mock[AutoCloseable]
+      doThrow(ex) when resource close ()
+      intercept[Exception] (loan (resource) to { _ => throw new Exception }).getSuppressed should equal (Array(ex))
+    }
+
+    "support the documented concise use case" in {
+      val out = new ByteArrayOutputStream
+      Loan(new PrintWriter(out)) { w: PrintWriter => w.println("Hello world!") }
+    }
+
+    "support the documented readable use case" in {
+      import Loan._
+      val out = new ByteArrayOutputStream
+      loan (new PrintWriter(out)) to { w: PrintWriter => w.println("Hello world!") }
+    }
+  }
+}
