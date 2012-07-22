3a367d487abe6a5c941c37c26adabf1fcb32cf62
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-07-22 23:49:26 +0200

    Renamed truecommons-logging to truecommons-log.

--- truecommons-log/src/main/java/de/schlichtherle/truecommons/log/LocalizedLogger.java
+++ truecommons-log/src/main/java/de/schlichtherle/truecommons/log/LocalizedLogger.java
@@ -2,7 +2,7 @@
  * Copyright (C) 2012 Schlichtherle IT Services.
  * All rights reserved. Use is subject to license terms.
  */
-package de.schlichtherle.truecommons.logging;
+package de.schlichtherle.truecommons.log;
 
 import java.text.MessageFormat;
 import java.util.Formatter;
