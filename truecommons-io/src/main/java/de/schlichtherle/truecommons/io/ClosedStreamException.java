4679e23495378a0c46ce5e1e716b66633df37dee
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-07-25 12:37:22 +0200

    Moved classes from TrueVFS Kernel Specification.

---
+++ truecommons-io/src/main/java/de/schlichtherle/truecommons/io/ClosedStreamException.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2005-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+package de.schlichtherle.truecommons.io;
+
+import java.io.IOException;
+import java.nio.channels.ClosedChannelException;
+import javax.annotation.concurrent.ThreadSafe;
+
+/**
+ * Indicates that an input or output stream has been closed.
+ *
+ * @see    ClosedChannelException
+ * @author Christian Schlichtherle
+ */
+@ThreadSafe
+public class ClosedStreamException extends IOException {
+    private static final long serialVersionUID = 3438364894193766238L;
+}
