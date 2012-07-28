a5a716a3a55e0b6dad672db65cc5606bed090cf7
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-07-28 18:11:47 +0200

    Improved performance.

--- truecommons-shed/src/main/java/de/schlichtherle/truecommons/shed/ControlFlowException.java
+++ truecommons-shed/src/main/java/de/schlichtherle/truecommons/shed/ControlFlowException.java
@@ -10,7 +10,8 @@ import javax.annotation.concurrent.Immutable;
 /**
  * Indicates a condition which requires non-local control flow.
  * Note that this class is an {@code Error} rather than a
- * {@link RuntimeException} just to prevent it from being accidentally catched.
+ * {@link RuntimeException} in order to prevent it from being accidentally
+ * catched.
  * 
  * @author Christian Schlichtherle
  */
@@ -18,15 +19,8 @@ import javax.annotation.concurrent.Immutable;
 @SuppressWarnings("serial") // serializing control flow exceptions is nonsense!
 public class ControlFlowException extends Error {
 
-    private static final String TRACEABLE_PROPERTY_KEY
-            = ControlFlowException.class.getName() + ".traceable";
-
-    public ControlFlowException() { this(null); }
-
-    public ControlFlowException(final @CheckForNull Throwable cause) {
-        // Don't disable suppression!
-        super(null != cause ? cause.toString() : null, cause, true, isTraceable());
-    }
+    private static final String TRACEABLE_PROPERTY_KEY =
+            ControlFlowException.class.getName() + ".traceable";
 
     /**
      * Returns {@code true} if and only if a control flow exception should have
@@ -41,7 +35,13 @@ public class ControlFlowException extends Error {
      * @return {@code true} if and only if a control flow exception should have
      *         a full stack trace instead of an empty stack trace.
      */
-    public static boolean isTraceable() {
-        return Boolean.getBoolean(TRACEABLE_PROPERTY_KEY);
+    private static final boolean TRACEABLE =
+            Boolean.getBoolean(TRACEABLE_PROPERTY_KEY);
+
+    public ControlFlowException() { this(null); }
+
+    public ControlFlowException(final @CheckForNull Throwable cause) {
+        // Don't disable suppression!
+        super(null != cause ? cause.toString() : null, cause, true, TRACEABLE);
     }
 }
