2e7e2ec61cfc76aeb27af2d097a61ca11b75a6fa
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-08-17 20:45:56 +0200

    Updated package-info.

--- truecommons-logging/src/main/java/net/java/truecommons/logging/package-info.java
+++ truecommons-logging/src/main/java/net/java/truecommons/logging/package-info.java
@@ -2,5 +2,8 @@
  * Copyright (C) 2012 Schlichtherle IT Services.
  * All rights reserved. Use is subject to license terms.
  */
-@edu.umd.cs.findbugs.annotations.DefaultAnnotation(edu.umd.cs.findbugs.annotations.NonNull.class)
+/**
+ * @author Christian Schlichtherle
+ */
+@javax.annotation.Nonnull @javax.annotation.ParametersAreNonnullByDefault
 package net.java.truecommons.logging;
