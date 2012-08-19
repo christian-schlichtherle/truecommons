5b66d9c2f3707663003451698dd9bbe3430e7aa1
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-08-19 19:34:10 +0200

    Updated documentation.

--- truecommons-services/src/main/java/net/java/truecommons/services/package-info.java
+++ truecommons-services/src/main/java/net/java/truecommons/services/package-info.java
@@ -3,6 +3,10 @@
  * All rights reserved. Use is subject to license terms.
  */
 /**
+ * Provides service location which is designed to work well in both JEE and
+ * OSGi environments and perfectly fits the dependency injection idiom with
+ * support for JSR 330.
+ * 
  * @author Christian Schlichtherle
  */
 @javax.annotation.Nonnull @javax.annotation.ParametersAreNonnullByDefault
