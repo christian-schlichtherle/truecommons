83d47aa3728251acbf3898ad948a57db9b995ed8
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-08-19 19:34:01 +0200

    Updated documentation.

--- truecommons-logging/src/main/java/net/java/truecommons/logging/package-info.java
+++ truecommons-logging/src/main/java/net/java/truecommons/logging/package-info.java
@@ -3,6 +3,8 @@
  * All rights reserved. Use is subject to license terms.
  */
 /**
+ * Provides a simple adapter to add support for localized messages to SLF4J.
+ * 
  * @author Christian Schlichtherle
  */
 @javax.annotation.Nonnull @javax.annotation.ParametersAreNonnullByDefault
