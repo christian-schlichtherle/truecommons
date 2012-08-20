f0e5650b460b854411d2923daf0d7d205ef11e65
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-08-20 09:50:37 +0200

    Improved documentation.

--- truecommons-shed/src/main/java/net/java/truecommons/shed/ControlFlowException.java
+++ truecommons-shed/src/main/java/net/java/truecommons/shed/ControlFlowException.java
@@ -4,19 +4,44 @@
  */
 package net.java.truecommons.shed;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import javax.annotation.CheckForNull;
 import javax.annotation.concurrent.Immutable;
 
 /**
  * Indicates a condition which requires non-local control flow.
- * Note that this class is an {@code Error} rather than a
- * {@link RuntimeException} in order to prevent it from being accidentally
- * catched.
+ * A control flow exception needs to get catched and resolved by the same
+ * thread which has thrown it, ideally within the same sub-system so that it
+ * doesn't accidentally "surprise" any foreign code.
+ * This implies that a control flow exception <em>must not</em> get passed on
+ * to other threads or even other JVMs, so serialization is deliberately not
+ * supported.
+ * <p>
+ * Note that although the name of this class suggests that it's an exception
+ * type, it deliberately subclasses {@code Error} in order to prevent its
+ * instances from being accidentally catched as an ordinary {@link Exception}.
+ * This is done to particularly protect against code which catches and
+ * erroneously suppresses any {@link RuntimeException} instead of propagating
+ * them to the caller.
+ * <p>
+ * As a general recommendation, if you need to ensure resource cleanup
+ * regardless of any {@code Throwable} then please place the cleanup code in a
+ * finally-block like this:
+ * <pre>{@code
+ * Resource resource = new Resource();
+ * try {
+ *     // May terminate with any Throwable, including ControlFlowException.
+ *     resource.use();
+ * } finally {
+ *     resource.cleanup(); // finally-block ensures cleanup
+ * }
+ * }</pre>
  * 
  * @author Christian Schlichtherle
  */
 @Immutable
 @SuppressWarnings("serial") // serializing control flow exceptions is nonsense!
+@SuppressFBWarnings("NM_CLASS_NOT_EXCEPTION")
 public class ControlFlowException extends Error {
 
     private static final String TRACEABLE_PROPERTY_KEY =
