3e016f29c02cfeeba848a98f85436d5ce068eebb
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-10-06 18:12:20 +0200

    Updated Javadoc and added unit test.

---
+++ truecommons-shed/src/test/java/net/java/truecommons/shed/StreamTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2005-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+package net.java.truecommons.shed;
+
+import java.util.Collections;
+import java.util.Iterator;
+import org.junit.Test;
+
+/**
+ * @author Christian Schlichtherle
+ */
+public class StreamTest {
+
+    @Test
+    public void testStream() {
+        TestContainer container = new TestContainer();
+        try (TestStream stream = container.stream()) {
+            for (Object object : stream) {
+                throw new AssertionError(object);
+            }
+        }
+    }
+
+    private class TestContainer {
+        TestStream stream() { return new TestStream(); }
+    } // Container
+
+    private class TestStream implements Stream<Object> {
+        @Override
+        public Iterator<Object> iterator() {
+            return Collections.emptyIterator();
+        }
+
+        @Override
+        public void close() {
+        }
+    } // TestStream
+}
