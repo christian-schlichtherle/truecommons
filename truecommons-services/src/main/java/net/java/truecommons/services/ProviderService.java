735062cddf3ecd02971c9d25f5d76d8eed48c5e0
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-10-14 21:28:18 +0200

    Updated Javadoc.

--- truecommons-services/src/main/java/net/java/truecommons/services/ProviderService.java
+++ truecommons-services/src/main/java/net/java/truecommons/services/ProviderService.java
@@ -11,9 +11,9 @@ import javax.inject.Provider;
  * An abstract service which provides products.
  * Provider services are subject to service location by {@link Locator}s.
  * <p>
- * If multiple provider services are locatable on the class path at run
- * time, a locator will select the provider service with the greatest
- * {@linkplain #getPriority() priority}.
+ * If multiple provider services are locatable on the class path at run time,
+ * the service with the greatest {@linkplain #getPriority() priority} gets
+ * selected.
  * <p>
  * Implementations should be thread-safe.
  *
