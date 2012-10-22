0ec74f9e0fe809883194d2b0bf55012a59a2427e
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-10-22 17:54:12 +0200

    Added MutableBuffer and ImmutableBuffer.

---
+++ truecommons-io/src/test/scala/net/java/truecommons/io/PowerBufferSpec.scala
@@ -0,0 +1,240 @@
+/*
+ * Copyright (C) 2005-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+package net.java.truecommons.io
+
+import java.nio._
+import java.nio.ByteOrder._
+import org.junit.runner._
+import org.scalatest._
+import org.scalatest.matchers._
+import org.scalatest.junit._
+
+/** @author Christian Schlichtherle */
+@RunWith(classOf[JUnitRunner])
+class PowerBufferSpec
+extends WordSpec with ShouldMatchers with ParallelTestExecution {
+
+  "A power buffer" when {
+    "allocated on the heap" should {
+      val pb = PowerBuffer allocate 1
+
+      "provide access to its adapted byte buffer" in {
+        pb.buffer should not be (null)
+      }
+
+      "not be direct" in {
+        pb.isDirect should be (false)
+      }
+
+      "provide access to its underlying array" in {
+        pb.array should not be (null)
+      }
+
+      "be a mutable adapter of its byte buffer" in {
+        testAdapter(pb)
+      }
+
+      "provide mutable and immutable views" in {
+        testViews(pb)
+      }
+    }
+
+    "not allocated on the heap" should {
+      val pb = PowerBuffer allocateDirect 1
+
+      "provide access to its adapted byte buffer" in {
+        pb.buffer should not be (null)
+      }
+
+      "be direct" in {
+        pb.isDirect should be (true)
+      }
+
+      "not provide access to its underlying array" in {
+        intercept[UnsupportedOperationException] { pb.array }
+      }
+
+      "be a mutable adapter of its byte buffer" in {
+        testAdapter(pb)
+      }
+
+      "provide mutable and immutable views" in {
+        testViews(pb)
+      }
+    }
+
+    "wrapping an array" should {
+      val array = new Array[Byte](1)
+      val pb = PowerBuffer wrap array
+
+      "provide access to its adapted byte buffer" in {
+        pb.buffer should not be (null)
+      }
+
+      "not be direct" in {
+        pb.isDirect should be (false)
+      }
+
+      "provide access to its underlying array" in {
+        pb.array should (be theSameInstanceAs (array))
+      }
+
+      "be a mutable adapter of its byte buffer" in {
+        testAdapter(pb)
+      }
+
+      "provide mutable and immutable views" in {
+        testViews(pb)
+      }
+    }
+
+    "partially wrapping an array" should {
+      val array = new Array[Byte](3)
+      val pb = PowerBuffer wrap (array, 1, 1)
+
+      "provide access to its adapted byte buffer" in {
+        pb.buffer should not be (null)
+      }
+
+      "not be direct" in {
+        pb.isDirect should be (false)
+      }
+
+      "provide access to its underlying array" in {
+        pb.array should (be theSameInstanceAs (array))
+      }
+
+      "be a mutable adapter of its byte buffer" in {
+        testAdapter(pb)
+      }
+
+      "provide mutable and immutable views" in {
+        testViews(pb)
+      }
+    }
+
+    "wrapping a byte buffer" should {
+      val buf = ByteBuffer allocate 1
+      val pb = PowerBuffer wrap buf
+
+      "provide access to its adapted byte buffer" in {
+        pb.buffer should be theSameInstanceAs (buf)
+      }
+
+      "not be direct" in {
+        pb.isDirect should be (false)
+      }
+
+      "provide access to its underlying array" in {
+        pb.array should not be (null)
+      }
+
+      "be a mutable adapter of its byte buffer" in {
+        testAdapter(pb)
+      }
+
+      "provide mutable and immutable views" in {
+        testViews(pb)
+      }
+    }
+
+    "viewed as mutable and read-only" should {
+      val pb = PowerBuffer allocate 1
+      val rob = pb.asMutableBuffer.asReadOnlyBuffer
+
+      "not support write access" in {
+        intercept[UnsupportedOperationException] { rob put (0, 0) }
+      }
+
+      "not return itself when asked to be set read-only again" in {
+        rob.asReadOnlyBuffer should not be theSameInstanceAs (rob)
+      }
+    }
+
+    "viewed as immutable and read-only" should {
+      val pb = PowerBuffer allocate 1
+      val rob = pb.asImmutableBuffer.asReadOnlyBuffer
+
+      "not support write access" in {
+        intercept[UnsupportedOperationException] { rob put (0, 0) }
+      }
+
+      "return itself when asked to be set read-only again" in {
+        rob.asReadOnlyBuffer should be theSameInstanceAs (rob)
+      }
+    }
+
+    "set to little-endian" should {
+      val bb = (MutableBuffer allocate 1).littleEndian.asImmutableBuffer.asReadOnlyBuffer
+
+      "retain this byte order" when {
+        "sliced" in {
+          bb.slice.order() should be (LITTLE_ENDIAN)
+        }
+
+        "duplicated" in {
+          bb.duplicate.order() should be (LITTLE_ENDIAN)
+        }
+
+        "viewed as read-only" in {
+          bb.asReadOnlyBuffer.order() should be (LITTLE_ENDIAN)
+        }
+
+        "viewed as a char buffer" in {
+          bb.asCharBuffer.order() should be (LITTLE_ENDIAN)
+        }
+
+        "viewed as a short buffer" in {
+          bb.asShortBuffer.order() should be (LITTLE_ENDIAN)
+        }
+
+        "viewed as an int buffer" in {
+          bb.asIntBuffer.order() should be (LITTLE_ENDIAN)
+        }
+
+        "viewed as a long buffer" in {
+          bb.asLongBuffer.order() should be (LITTLE_ENDIAN)
+        }
+
+        "viewed as a float buffer" in {
+          bb.asFloatBuffer.order() should be (LITTLE_ENDIAN)
+        }
+
+        "viewed as a double buffer" in {
+          bb.asDoubleBuffer.order() should be (LITTLE_ENDIAN)
+        }
+      }
+    }
+  }
+
+  def testAdapter(pb: PowerBuffer[_]) {
+    val buf = pb.buffer
+    pb position 1
+    buf.position should be (1)
+    buf position 0
+    pb.position should be (0)
+  }
+
+  def testViews(pb: PowerBuffer[_]) {
+    pb position 0
+
+    val mb = pb.asMutableBuffer
+    mb should be theSameInstanceAs (pb)
+
+    val ib = pb.asImmutableBuffer
+    ib should not be theSameInstanceAs (pb)
+
+    mb.asMutableBuffer should be theSameInstanceAs (mb)
+    ib.asImmutableBuffer should be theSameInstanceAs (ib)
+    mb.asImmutableBuffer should not be theSameInstanceAs (mb)
+    ib.asMutableBuffer should not be theSameInstanceAs (ib)
+
+    mb position 1
+    ib.position() should be (0)
+    intercept[UnsupportedOperationException] { ib position 1 }
+    ib.asMutableBuffer position 1
+    ib.position() should be (0)
+  }
+}
