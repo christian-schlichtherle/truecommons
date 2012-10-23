5e10e4f26204642eead51d7d70a7b112e2b08996
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-10-23 10:52:38 +0200

    RCB

--- truecommons-io/src/main/java/net/java/truecommons/io/PowerBuffer.java
+++ truecommons-io/src/main/java/net/java/truecommons/io/PowerBuffer.java
@@ -139,8 +139,7 @@ implements Comparable<This>, Cloneable {
         bb.mark();
         do {
             int read = channel.read(bb);
-            if (0 > read)
-                throw new EOFException();
+            if (0 > read) throw new EOFException();
             remaining -= read;
         } while (0 < remaining);
         bb.reset();
