3a20004e7a7bc1c30db9a8224d854e5ed3eb25df
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-10-23 17:51:19 +0200

    Added TrueCommons Container I/O.

---
+++ truecommons-cio/src/main/java/net/java/truecommons/cio/InputBusyException.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2005-2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+package net.java.truecommons.cio;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.concurrent.ThreadSafe;
+
+/**
+ * Indicates that an entity (an entry or container) could not get read
+ * because the entity or its container is busy.
+ * This exception is recoverable, meaning it should be possible to repeat the
+ * operation successfully as soon as the entity or its container is not busy
+ * anymore and no other exceptional conditions apply.
+ *
+ * @see    OutputBusyException
+ * @author Christian Schlichtherle
+ */
+@ThreadSafe
+public class InputBusyException extends BusyException {
+    private static final long serialVersionUID = 1983745618753823654L;
+
+    public InputBusyException(@CheckForNull String message) {
+        super(message);
+    }
+
+    public InputBusyException(@CheckForNull Throwable cause) {
+        super(cause);
+    }
+}
