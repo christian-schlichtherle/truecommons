66c427a8eb2d155b1f8f9054976c3a323f192ae8
Christian Schlichtherle <1545428+christian-schlichtherle@users.noreply.github.com>
2012-10-27 17:24:20 +0200

    Added stub for New-Scala-Compiler plugin to process @Service(Specification|Implementation) annotations.

---
+++ truecommons-services/src/main/scala/net/java/truecommons/services/nscp/ServiceSpecificationPlugin.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+package net.java.truecommons.services.nscp
+
+import scala.tools.nsc._
+import scala.tools.nsc.plugins._
+
+/** @author Christian Schlichtherle */
+class ServiceSpecificationPlugin(val global: Global) extends Plugin { plugin =>
+
+  val name = "ServiceSpecificationPlugin"
+  val description = "Processes the @ServiceSpecification annotation."
+  val components = List[PluginComponent](Component)
+
+  object Component extends PluginComponent {
+
+    val global: plugin.global.type = plugin.global
+    val phaseName = "processServiceSpecificationAnnotation"
+    val runsAfter = List("refchecks")
+
+    def newPhase(phase: Phase): Phase = new ServiceSpecificationPhase(phase)
+
+    class ServiceSpecificationPhase(phase: Phase) extends StdPhase(phase) {
+
+      import global._
+
+      def apply(unit: CompilationUnit) {
+        println("Hello World!")
+      }
+    }
+  }
+}
