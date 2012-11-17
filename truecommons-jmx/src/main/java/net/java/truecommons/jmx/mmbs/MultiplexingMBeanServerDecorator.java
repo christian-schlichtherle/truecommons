5ef7240c0dfaa6d8eb3fa344509e5cd68fc084fa
Christian Schlichtherle <christian AT schlichtherle DOT de@localhost>
2012-11-17 11:42:54 +0100

    Removed dependency on Scala Library.

---
+++ truecommons-jmx/src/main/java/net/java/truecommons/jmx/mmbs/MultiplexingMBeanServerDecorator.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2012 Schlichtherle IT Services.
+ * All rights reserved. Use is subject to license terms.
+ */
+package net.java.truecommons.jmx.mmbs;
+
+import javax.annotation.concurrent.Immutable;
+import javax.management.MBeanServer;
+import net.java.truecommons.annotations.ServiceImplementation;
+import net.java.truecommons.jmx.sl.ObjectNameModifierLocator;
+import net.java.truecommons.jmx.spi.MBeanServerDecorator;
+
+/**
+ * Decorates the given MBean server with a {@link MultiplexingMBeanServer} if
+ * and only if this class is <em>not</em> defined by the system class loader.
+ * 
+ * @since  TrueCommons 2.3
+ * @author Christian Schlichtherle
+ */
+@Immutable
+@ServiceImplementation
+public final class MultiplexingMBeanServerDecorator
+extends MBeanServerDecorator {
+
+    @Override
+    public MBeanServer apply(final MBeanServer mbs) {
+        return getClass().getClassLoader() == ClassLoader.getSystemClassLoader()
+                ? mbs
+                : new MultiplexingMBeanServer(mbs, ObjectNameModifierLocator.SINGLETON.get());
+    }
+
+    /** @return -100 */
+    @Override
+    public int getPriority() { return -100; }
+}
