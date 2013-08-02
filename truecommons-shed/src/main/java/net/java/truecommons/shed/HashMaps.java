d818708740cc8a8aec860a56b5d404be244d5630
Christian Schlichtherle <christian@schlichtherle.de>
2013-08-02 11:46:20 +0200

    Updated Javadoc.

--- truecommons-shed/src/main/java/net/java/truecommons/shed/HashMaps.java
+++ truecommons-shed/src/main/java/net/java/truecommons/shed/HashMaps.java
@@ -7,8 +7,8 @@ package net.java.truecommons.shed;
 import javax.annotation.concurrent.Immutable;
 
 /**
- * Static utility methods for hash maps.
- * 
+ * Utility functions for hash maps.
+ *
  * @author Christian Schlichtherle
  */
 @Immutable
@@ -19,7 +19,7 @@ public final class HashMaps {
      * hash map with a load factor of 75% before resizing it, which is {@value}.
      * When a new hash map gets created, this constant should get used in order
      * to compute the initial capacity or overhead for additional entries.
-     * 
+     *
      * @see   #initialCapacity(int)
      */
     public static final int OVERHEAD_SIZE = (64 - 1) * 3 / 4; // consider 75% load factor
@@ -28,7 +28,7 @@ public final class HashMaps {
 
     /**
      * Returns the initial capacity for a hash table with a load factor of 75%.
-     * 
+     *
      * @param  size the number of entries to accommodate space for.
      * @return The initial capacity for a hash table with a load factor of 75%.
      * @see    #OVERHEAD_SIZE
