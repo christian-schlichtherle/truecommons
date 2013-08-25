432061cf3b75954b88aa68e684a135d5b2a6d548
Christian Schlichtherle <christian@schlichtherle.de>
2013-08-25 20:14:27 +0200

    RCB

--- truecommons-shed/src/main/java/net/java/truecommons/shed/HashMaps.java
+++ truecommons-shed/src/main/java/net/java/truecommons/shed/HashMaps.java
@@ -12,7 +12,7 @@ import javax.annotation.concurrent.Immutable;
  * @author Christian Schlichtherle
  */
 @Immutable
-public final class HashMaps {
+public class HashMaps {
 
     /**
      * The number of entries which should be additionally accomodatable by a
