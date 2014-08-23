a1dc1b8a34c2c0ea16d3388afab823c5f44088e7
Christian Schlichtherle <christian@schlichtherle.de>
2014-08-23 14:10:20 +0200

    Documentation.

--- truecommons-jmx/src/main/java/net/java/truecommons/jmx/mmbs/MultiplexingMBeanServerDecorator.java
+++ truecommons-jmx/src/main/java/net/java/truecommons/jmx/mmbs/MultiplexingMBeanServerDecorator.java
@@ -13,7 +13,7 @@ import net.java.truecommons.jmx.spi.MBeanServerDecorator;
 /**
  * Decorates the given MBean server with a {@link MultiplexingMBeanServer} if
  * and only if this class is <em>not</em> defined by the system class loader.
- * 
+ *
  * @since  TrueCommons 2.3
  * @author Christian Schlichtherle
  */
@@ -24,9 +24,14 @@ extends MBeanServerDecorator {
 
     @Override
     public MBeanServer apply(final MBeanServer mbs) {
-        return getClass().getClassLoader() == ClassLoader.getSystemClassLoader()
-                ? mbs
-                : new MultiplexingMBeanServer(mbs, ObjectNameModifierLocator.SINGLETON.get());
+        // Iff this code is running in a container like Tomcat where each app
+        // is loaded by a different class loader, then multiplex the object
+        // instances by modifying their object names in a unique fashion.
+        // This allows multiple container apps to register their object
+        // instances with an object name which would otherwise be equal.
+        return getClass().getClassLoader() != ClassLoader.getSystemClassLoader()
+                ? new MultiplexingMBeanServer(mbs, ObjectNameModifierLocator.SINGLETON.get())
+                : mbs;
     }
 
     /** @return -100 */
