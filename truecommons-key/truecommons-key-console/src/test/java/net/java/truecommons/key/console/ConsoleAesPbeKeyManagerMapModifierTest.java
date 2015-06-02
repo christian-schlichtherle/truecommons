e51e78fa04cfba519b585dd487f613233bf0f4e2
Christian Schlichtherle <christian@schlichtherle.de>
2015-06-02 21:58:51 +0200

    Makes static code analysis happy.

--- truecommons-key/truecommons-key-console/src/test/java/net/java/truecommons/key/console/ConsoleAesPbeKeyManagerMapModifierTest.java
+++ truecommons-key/truecommons-key-console/src/test/java/net/java/truecommons/key/console/ConsoleAesPbeKeyManagerMapModifierTest.java
@@ -2,13 +2,13 @@
  * Copyright (C) 2005-2015 Schlichtherle IT Services.
  * All rights reserved. Use is subject to license terms.
  */
-package net.java.truecommon.key.console;
+package net.java.truecommons.key.console;
 
-import net.java.truecommons.key.console.ConsoleAesPbeKeyManagerMapModifier;
-import java.util.Arrays;
 import net.java.truecommons.key.spec.common.AesPbeParameters;
 import net.java.truecommons.key.spec.spi.KeyManagerMapModifier;
 import net.java.truecommons.key.spec.spi.KeyManagerMapModifierTestSuite;
+
+import java.util.Collections;
 
 /**
  * @author Christian Schlichtherle
@@ -19,7 +19,7 @@ extends KeyManagerMapModifierTestSuite {
     @Override
     @SuppressWarnings("unchecked")
     protected Iterable<Class<?>> getClasses() {
-        return (Iterable<Class<?>>) Arrays.asList((Class<?>) AesPbeParameters.class);
+        return Collections.<Class<?>>singleton(AesPbeParameters.class);
     }
 
     @Override
